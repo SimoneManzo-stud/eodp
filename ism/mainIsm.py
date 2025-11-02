3251ce1a234c812b7d0fee1c6812c17d43f9d2c4
simonci1 <manzosimone03@gmail.com>
2025-11-02 16:18:30 +0100

    Directory adjustments

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -6,7 +6,7 @@ from ism.src.ism import ism
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\\Users\\Simone\\OneDrive\\Documenti\\Desktop\\EODP\\eodp\\auxiliary'
 indir = r"C:\\Users\\Simone\\OneDrive\\Documenti\\Desktop\\Earth Observation\\EODP-TS-E2E-20251002T100205Z-1-001\\EODP-TS-E2E\\sgm_out"# small scene
-outdir = r'C:\\Users\\Simone\\OneDrive\\Documenti\Desktop\\my_output_ism1610'
+outdir = r'C:\\Users\\Simone\\OneDrive\\Documenti\\Desktop\\myoutputs\\myoutput_ism_1610'
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -5,8 +5,8 @@ from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\\Users\\Simone\\OneDrive\\Documenti\\Desktop\\EODP\\eodp\\auxiliary'
-indir = r'C:\\Users\\Simone\\OneDrive\\Documenti\Desktop\\my_output_ism1610'
-outdir = r"C:\\Users\\Simone\\OneDrive\\Documenti\\Desktop\\myoutput_L1B_1610"
+indir =  r'C:\Users\Simone\OneDrive\Documenti\Desktop\myoutputs\myoutput_ism_1610'
+outdir = r"C:\\Users\\Simone\\OneDrive\\Documenti\\Desktop\\myoutputs\\myoutput_L1B_1610"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
